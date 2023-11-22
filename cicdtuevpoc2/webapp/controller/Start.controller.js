c68970f7ad0eea59e9dc881ccfce92a075ff5f7b
Maxim Fuchs <maxim.fuchs@rewion.com>
2023-11-22 12:51:17 +0000

    initial commit

---
+++ cicdtuevpoc2/webapp/controller/Start.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("cicdtuevpoc2.controller.Start", {
+            onInit: function () {
+
+            }
+        });
+    });
---
+++ cicdtuevpoc2/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"cicdtuevpoc2/test/unit/controller/Start.controller"
+], function () {
+	"use strict";
+});
